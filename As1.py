38fe4dd3b35a13aefde39421ff138431d8921f2c
TemurKhabibullaev <55756791+TemurKhabibullaev@users.noreply.github.com>
2020-02-05 17:48:56 -0500

    Done

--- As1.py
+++ As1.py
@@ -1,4 +1,5 @@
 # Temur Khabibullaev As1
+import pyttsx3
 string = 'word'
 print(len(string))
 print(string[0])
@@ -12,8 +13,20 @@ else:
     print("We messed up.")
 y = "Hello World I am Temur"
 print(y.split()[0])
+
 # Download pyttsx3 library first! Also stay connected to the internet you might will need it
-import pyttsx3
+# Sorry if it's out of topic!
 engine = pyttsx3.init()
-engine.say("Hello my creator was Temur. I see a great potential in my creator! I wish him luck and success in achieving all of his goals!")
+new_string = "Hello World. My creator is Temur. I see a great potential in my creator! " \
+             "I wish him luck and success in achieving all of his goals!"
+engine.say(new_string)
+engine.say("If you want me to speak something then just type it in now! Say bye when you are done")
 engine.runAndWait()
+while True:
+    audio = input()
+    engine.say(audio)
+    engine.runAndWait()
+    if audio == "bye":
+        engine.say("Have a good one!")
+        engine.runAndWait()
+        break
